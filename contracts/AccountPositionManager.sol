32875d2cd6145d87968c823de4c7235b0e84b081
phuccaliber <phuc@caliber.build>
2025-05-23 11:28:31 +0700

    feat(account_position): create empty account position manager

---
+++ contracts/AccountPositionManager.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "./interfaces/IAccountPositionManager.sol";
+
+contract AccountPositionManager is IAccountPositionManager {}
