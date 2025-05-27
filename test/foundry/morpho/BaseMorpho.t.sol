5f08cbfa3186744280cd6bc0f09242aa637ad917
phuccaliber <phuc@caliber.build>
2025-05-27 14:54:51 +0700

    feat(morpho): deploy morpho then test to avoid solidity conflict version

--- test/foundry/morpho/BaseMorpho.t.sol
+++ test/foundry/morpho/BaseMorpho.t.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import "../../../contracts/AccountPositionManager.sol";
-import {Morpho} from "../../../lib/metamorpho-v1.1/lib/morpho-blue/src/Morpho.sol";
+import {IMorpho} from "../../../lib/metamorpho-v1.1/lib/morpho-blue/src/interfaces/IMorpho.sol";
 import "../../mock/ERC20Mock.sol";
 import "../../mock/interfaces/IERC20Mock.sol";
 
 contract BaseMorphoTest is Test {
-    Morpho public MORPHO;
+    IMorpho public MORPHO;
     IERC20Mock public BTC;
     IERC20Mock public USDC;
     address public OWNER;
@@ -17,11 +17,15 @@ contract BaseMorphoTest is Test {
     address public MPC;
 
     function setUp() public {
+        vm.createSelectFork("http://localhost:8545");
+        string memory path = string.concat(vm.projectRoot(), "/deployments/morpho.json");
+        string memory json = vm.readFile(path);
+        address morphoAddress = vm.parseJsonAddress(json, ".address");
+        MORPHO = IMorpho(morphoAddress);
         OWNER = makeAddr("OWNER");
         SUPPLIER = makeAddr("SUPPLIER");
         BORROWER = makeAddr("BORROWER");
         MPC = makeAddr("MPC");
-        MORPHO = new Morpho(OWNER);
         BTC = IERC20Mock(new MockERC20("BTC", "BTC", 8));
         USDC = IERC20Mock(new MockERC20("USDC", "USDC", 6));
         BTC.mint(BORROWER, 1e8);
