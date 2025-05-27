4b72a53ac6ae476dc1b0ba7f119ee3536b82dd77
phuccaliber <phuc@caliber.build>
2025-05-27 14:58:01 +0700

    feat(build): use solidity version 0.8.20

--- test/foundry/account_position/AccountPositionManagerFactory.t.sol
+++ test/foundry/account_position/AccountPositionManagerFactory.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import "../../../lib/forge-std/src/Test.sol";
 import "../../../contracts/AccountPositionManagerFactory.sol";
