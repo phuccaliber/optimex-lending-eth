355a40840888e74de5d37b145357d9cde1e64b6b
phuccaliber <phuc@caliber.build>
2025-05-27 13:51:07 +0700

    feat(morpho): setup morpho with mock token

--- test/foundry/morpho/BaseMorpho.t.sol
+++ test/foundry/morpho/BaseMorpho.t.sol
@@ -2,14 +2,29 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
-import "../../../lib/metamorpho-v1.1/lib/morpho-blue/src/Morpho.sol";
+import "../../../contracts/AccountPositionManager.sol";
+import {Morpho} from "../../../lib/metamorpho-v1.1/lib/morpho-blue/src/Morpho.sol";
+import "../../mock/ERC20Mock.sol";
+import "../../mock/interfaces/IERC20Mock.sol";
 
 contract BaseMorphoTest is Test {
     Morpho public MORPHO;
+    IERC20Mock public BTC;
+    IERC20Mock public USDC;
     address public OWNER;
+    address public SUPPLIER;
+    address public BORROWER;
+    address public MPC;
 
     function setUp() public {
         OWNER = makeAddr("OWNER");
+        SUPPLIER = makeAddr("SUPPLIER");
+        BORROWER = makeAddr("BORROWER");
+        MPC = makeAddr("MPC");
         MORPHO = new Morpho(OWNER);
+        BTC = IERC20Mock(new MockERC20("BTC", "BTC", 8));
+        USDC = IERC20Mock(new MockERC20("USDC", "USDC", 6));
+        BTC.mint(BORROWER, 1e8);
+        USDC.mint(SUPPLIER, 1000000e6);
     }
 }
--- test/foundry/morpho/BaseMorphoSetup.t.sol
+++ test/foundry/morpho/BaseMorphoSetup.t.sol
@@ -6,5 +6,12 @@ import {BaseMorphoTest} from "./BaseMorpho.t.sol";
 contract BaseMorphoSetupTest is BaseMorphoTest {
     function test_InitBaseMorpho() public view {
         assertNotEq(address(MORPHO), address(0), "Morpho blue is not initialized");
+        assertNotEq(address(BTC), address(0), "BTC is not initialized");
+        assertNotEq(address(USDC), address(0), "USDC is not initialized");
+    }
+
+    function test_InitBalanceSuccess() public view {
+        assertEq(BTC.balanceOf(BORROWER), 1e8, "BTC balance is not correct");
+        assertEq(USDC.balanceOf(SUPPLIER), 1000000e6, "USDC balance is not correct");
     }
 }
